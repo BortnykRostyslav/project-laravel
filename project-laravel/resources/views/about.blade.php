ac076a0174c3f45b8a49b6e5452914c171208406
Rostyslav <bortnikrostislav370@gmail.com>
2024-07-30 19:46:55 +0300

    Fix 4.2.3

--- project-laravel/resources/views/about.blade.php
+++ project-laravel/resources/views/about.blade.php
@@ -6,7 +6,7 @@
 
 @section('main_content')
     <div class="p-4 p-md-5 mb-4 rounded text-body-emphasis bg-body-secondary">
-        <h1 class="display-4 fst-italic">Page about</h1>
+        <h1 class="mt-5">Page about</h1>
         <p class="lead my-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, eaque itaque mollitia officia placeat possimus reiciendis. Corporis debitis dolore eius est fuga ipsa iusto odit perferendis soluta temporibus, tenetur veritatis?</p>
         <a class="btn btn-primary" href="/review" role="button">Review</a>
     </div>
--- project-laravel/resources/views/home.blade.php
+++ project-laravel/resources/views/home.blade.php
@@ -7,7 +7,7 @@
 
         <div class="p-4 p-md-5 mb-4 rounded text-body-emphasis bg-body-secondary">
             <div class="container">
-                <h1 class="display-4 fst-italic">Home page</h1>
+                <h1 class="mt-5">Home page</h1>
                 <p class="lead my-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A adipisci aliquid atque beatae dolorem ducimus fuga, saepe totam ullam voluptatibus?</p>
                 <p><a class="btn btn-primary" href="/review" role="button">Review </a></p>
             </div>
@@ -17,17 +17,17 @@
 
             <div class="row">
                 <div class="col-md-4">
-                    <h2 class="display-4 fst-italic">Heading</h2>
+                    <h2 class="mt-5">Heading</h2>
                     <p class="lead my-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus architecto delectus id in obcaecati optio praesentium quas quis reiciendis unde.</p>
                     <p><a class="btn btn-primary" href="#" role="button">View details</a></p>
                 </div>
                 <div class="col-md-4">
-                    <h2 class="display-4 fst-italic">Heading</h2>
+                    <h2 class="mt-5">Heading</h2>
                     <p class="lead my-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci eius laudantium magni odio quaerat quod rem tempora. Aperiam, deserunt maxime.</p>
                     <p><a class="btn btn-primary" href="#" role="button">View details</a></p>
                 </div>
                 <div class="col-md-4">
-                    <h2 class="display-4 fst-italic">Heading</h2>
+                    <h2 class="mt-5">Heading</h2>
                     <p class="lead my-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate dolorem dolorum explicabo incidunt inventore quae ratione recusandae repellat rerum sequi!</p>
                     <p><a class="btn btn-primary" href="#" role="button">View details</a></p>
                 </div>
